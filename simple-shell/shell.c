6014964ca7154e3bf0c2398b98eea5b18c2246c1
Felipe Pontes <felipemfpontes@gmail.com>
2017-05-12 08:23:34 -0300

    Add exit command

--- simple-shell/shell.c
+++ simple-shell/shell.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define COMMAND_SIZE 132
+#define EXIT_COMMAND "exit"
+
+void error(char * message) {
+  printf("error: %s\n", message);
+  exit(1);
+}
+
+void read_command(char * command) {
+  printf("$ ");
+  fgets(command, COMMAND_SIZE, stdin);
+  sscanf(command, "%[^\n]", command);
+}
 
 int main() {
-  printf("Hello World\n");
+  char command[COMMAND_SIZE + 2]; // \n + null
+
+  while (1) {
+    read_command(command);
+
+    /*
+     * handle
+     */
+    if (strcmp(command, EXIT_COMMAND) == 0) break;
+    else error("Command not found");
+  }
+
   return(0);
 }
