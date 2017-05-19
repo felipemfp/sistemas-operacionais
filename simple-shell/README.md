1d048a207f2258f6f10946bfe2eade1411de13b0
Felipe Pontes <felipemfpontes@gmail.com>
2017-05-19 16:17:58 -0300

    Add fork, exec and waitpid workflow

---
+++ simple-shell/README.md
@@ -0,0 +1,10 @@
+# Referências
+
+**fork() e exec()**
+
+- https://ece.uwaterloo.ca/~dwharder/icsrts/Tutorials/fork_exec/
+- http://www.dca.ufrn.br/~adelardo/cursos/DCA409/node39.html
+
+**waitpid()**
+
+- https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.bpxbd00/rtwaip.htm
--- simple-shell/shell.c
+++ simple-shell/shell.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #define COMMAND_SIZE 132
 #define EXIT_COMMAND "exit"
 
 void error(char * message) {
   printf("error: %s\n", message);
-  exit(1);
 }
 
 void read_command(char * command) {
@@ -17,7 +20,8 @@ void read_command(char * command) {
 }
 
 int main() {
-  char command[COMMAND_SIZE + 2]; // \n + null
+  char command[COMMAND_SIZE + 1];
+  pid_t child_pid;
 
   while (1) {
     read_command(command);
@@ -26,7 +30,22 @@ int main() {
      * handle
      */
     if (strcmp(command, EXIT_COMMAND) == 0) break;
-    else error("Command not found");
+    else {
+      child_pid = fork();
+      if (child_pid < 0) {
+        error("can't fork");
+      } else if (child_pid == 0) {
+        char *argv[] = { command, NULL };
+        _exit(execvp(command, argv));
+      } else {
+        int status;
+        if (waitpid(child_pid, &status, 0) == -1) {
+          error(strerror(errno));
+        } else if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+          error("command not found");
+        }
+      }
+    }
   }
 
   return(0);
